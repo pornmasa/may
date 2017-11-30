74aca379abd9d6699907d9332f7a88969aac817e
pornmasa Namtirach <maysaa142@gmail.com>
2017-11-30 13:13:49 +0700

    client.js
    ส่งเป็น json ยังไม่ได้

---
+++ client.js
@@ -0,0 +1,39 @@
+var http = require('http');
+var querystring = require('querystring');
+
+
+var postData = querystring.stringify({
+    msg: 'hello world'
+});
+
+var options = {
+    hostname: '127.0.0.1',
+    port: 90,
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': postData.length
+    }
+};
+
+var req = http.request(options, function (res) {
+    console.log('STATUS:', res.statusCode);
+    console.log('HEADERS:', JSON.stringify(res.headers));
+
+    res.setEncoding('utf8');
+
+    res.on('data', function (chunk) {
+        console.log('BODY:', chunk);
+    });
+
+    res.on('end', function () {
+        console.log('No more data in response.');
+    });
+});
+
+req.on('error', function (e) {
+    console.log('Problem with request:', e.message);
+});
+
+req.write(postData);
+req.end();
